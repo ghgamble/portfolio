5e52aa9ba2948f83dfb28483ddb35b1048b2e0c6
ghgamble <grace.h.gamble@gmail.com>
2024-01-25 11:14:16 -0700

    gulp for images and build commands

--- app/public/wp-content/themes/portfolio-theme/gulpfile.babel.js
+++ app/public/wp-content/themes/portfolio-theme/gulpfile.babel.js
@@ -5,8 +5,11 @@ import gulpSass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
 import gulpif from 'gulp-if';
 import sourcemaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin';
+import { deleteAsync } from 'del';
+import { hideBin } from 'yargs/helpers';
 
-const sass = gulpSass( dartSass );
+const sass = gulpSass(dartSass);
 
 const PRODUCTION = yargs(process.argv).argv.prod;
 
@@ -14,8 +17,18 @@ const paths = {
     styles: {
         src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'], 
         dest: 'dist/assets/css/'
+    },
+    images: {
+        src: 'src/assets/images/**/*.{jpg,jpeg,png,svg,gif}',
+        dest: 'dist/assets/images'
+    },
+    other: {
+        src: ['src/assets/**/*', '!src/assets/{images,js,scss}', '!src/assets/{images,js,scss/**/*}'],
+        dest: 'dist/assets'
     }
 };
+
+export const clean = () => deleteAsync(['dist']);
 
 export const styles = () => {
     return gulp.src(paths.styles.src)
@@ -23,11 +36,27 @@ export const styles = () => {
         .pipe(sass().on('error', sass.logError))
         .pipe(gulpif(PRODUCTION, cleanCSS({compatibility: 'ie8'})))
         .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
-        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(gulp.dest(paths.styles.dest));
+}
+
+export const images = () => {
+    return gulp.src(paths.images.src)
+        .pipe(gulpif(PRODUCTION, imagemin()))
+        .pipe(gulp.dest(paths.images.dest));
 }
 
 export const watch = () => {
-    gulp.watch('src/assets/scss/**/*.scss', styles)
+    gulp.watch('src/assets/scss/**/*.scss', styles);
+    gulp.watch(paths.images.src, images);
+    gulp.watch(paths.other.src, copy);
 }
 
-// export default hello;
+export const copy = () => {
+    return gulp.src(paths.other.src)
+        .pipe(gulp.dest(paths.other.dest));
+}
+
+export const dev = gulp.series(clean, gulp.parallel(styles, images, copy), watch);
+export const build = gulp.series(clean, gulp.parallel(styles, images, copy));
+
+export default dev;
