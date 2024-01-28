79c7fd4b69bb5c4b52c0857e6e7fd525c01adb05
ghgamble <grace.h.gamble@gmail.com>
2024-01-28 12:29:39 -0700

    footer widgets

--- app/public/wp-content/themes/portfolio-child/functions.php
+++ app/public/wp-content/themes/portfolio-child/functions.php
@@ -12,9 +12,9 @@ add_action('wp_enqueue_scripts', 'portfolio_child_scripts');
 // }
 // add_action('_themename_after_pagination', 'after_pagination');
 
-// function function_to_add($query) {
-//     if($query->is_main_query()) {
-//         $query->set('posts_per_page', 2);
-//     }
-// }
-// add_action('pre_get_posts','function_to_add', 10, 1);
+function function_to_add($query) {
+    if($query->is_main_query()) {
+        $query->set('posts_per_page', 3);
+    }
+}
+add_action('pre_get_posts','function_to_add', 10, 1);
