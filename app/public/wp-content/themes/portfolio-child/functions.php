a8c1204d20590bb37a7d255c38d1f7b40279233e
ghgamble <grace.h.gamble@gmail.com>
2024-01-28 10:23:37 -0700

    set up child theme directory files

--- app/public/wp-content/themes/portfolio-child/functions.php
+++ app/public/wp-content/themes/portfolio-child/functions.php
@@ -1,2 +1,20 @@
-<?php 
+<?php
 
+function portfolio_child_scripts() {
+    // get_template_directory_uri() will always point to parent theme, in child theme need to be more specific
+    wp_enqueue_style('portfolio-child-styles', get_stylesheet_directory_uri() . '/bundle.css', array('_themename-stylesheet'), '1.0.0', 'all');
+}
+add_action('wp_enqueue_scripts', 'portfolio_child_scripts');
+
+function after_pagination($x) {
+    echo $x;
+    echo 'test';
+}
+add_action('_themename_after_pagination', 'after_pagination');
+
+function function_to_add($query) {
+    if($query->is_main_query()) {
+        $query->set('posts_per_page', 2);
+    }
+}
+add_action('pre_get_posts','function_to_add', 10, 1);
--- app/public/wp-content/themes/portfolio/functions.php
+++ app/public/wp-content/themes/portfolio/functions.php
@@ -2,9 +2,3 @@
 
 require_once('lib/helpers.php');
 require_once('lib/enqueue-assets.php');
-
-function after_pagination() {
-    echo 'text';
-}
-
-add_action('_themename_after_pagination','after_pagination');
