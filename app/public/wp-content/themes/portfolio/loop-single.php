97783c33500f791572287f257f602704088f59bb
ghgamble <grace.h.gamble@gmail.com>
2024-02-05 12:36:13 -0700

    comments for single post

--- app/public/wp-content/themes/portfolio/loop-single.php
+++ app/public/wp-content/themes/portfolio/loop-single.php
@@ -10,6 +10,12 @@
         ?>
         <?php get_template_part('template-parts/single/navigation'); ?>
 
+        <?php 
+        if(comments_open() || get_comments_number()) {
+            comments_template();
+        }
+        ?>
+
     <?php } ?>
 <?php } else { ?>
     <?php get_template_part('template-parts/post/content', 'none'); ?>
