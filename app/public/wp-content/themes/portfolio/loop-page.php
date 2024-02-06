19e731bd940d94ad5550bbe00d3fb94ba41ae94c
ghgamble <grace.h.gamble@gmail.com>
2024-02-06 11:59:26 -0700

    custom templates

--- app/public/wp-content/themes/portfolio/loop-page.php
+++ app/public/wp-content/themes/portfolio/loop-page.php
@@ -4,12 +4,6 @@
         
         <?php get_template_part('template-parts/page/content'); ?>
 
-        <?php 
-        if(comments_open() || get_comments_number()) {
-            comments_template();
-        }
-        ?>
-
     <?php } ?>
 <?php } else { ?>
     <?php get_template_part('template-parts/post/content', 'none'); ?>
