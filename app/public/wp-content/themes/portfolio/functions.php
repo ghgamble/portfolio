436d4acc84f3c21640bf756822731909de9d9ad7
ghgamble <grace.h.gamble@gmail.com>
2024-02-08 11:33:49 -0700

    finish widget setup

--- app/public/wp-content/themes/portfolio/functions.php
+++ app/public/wp-content/themes/portfolio/functions.php
@@ -10,3 +10,6 @@ require_once('lib/comment-callback.php');
 require_once('lib/portfolio.php');
 require_once('lib/delete-post.php');
 require_once('lib/most-recent-widget.php');
+
+add_action('wp_head', function() { echo '<script type="text/javascript"> if (typeof(wp) == "undefined") { window.wp = { i18n: { setLocaleData: (function() { return false; })} }; } </script>'; });
+
--- app/public/wp-content/themes/portfolio/lib/most-recent-widget.php
+++ app/public/wp-content/themes/portfolio/lib/most-recent-widget.php
@@ -12,7 +12,7 @@ class _themename_Most_Recent_Widget extends WP_Widget {
 
     public function __construct() {
         parent::__construct(
-            '_themename_mst_recent_widget',
+            '_themename_most_recent_widget',
             esc_html__('Recent Posts', '_themename'),
             array(
                 'description' => esc_html__('some description', '_themename'),
